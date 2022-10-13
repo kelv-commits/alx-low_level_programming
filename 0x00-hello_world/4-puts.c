2783bd6649a21be5f2bb02878d6f534ed0677311
“kelv-commits” <kelvinonwuka8@gmail.com>
2022-10-13 10:10:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- * * main - Printing
- *
- * *
- *
- * * Return: Always 0 (Successful)
- *
- * */
-
+ * main - Entry
+ * return: Always 0
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-
 }
